eb32b83428999cdd5a6ac73822b1acb6b7625474
nagaoyosuke <hw17a124@oecu.jp>
2021-01-24 09:18:04 +0900

    リザルトのステージ実装

---
+++ Assets/Scripts/Result/ScoreView.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreView : MonoBehaviour
+{
+    [SerializeField]
+    UnityEngine.UI.Text ScoreText;
+
+    [SerializeField]
+    private bool isStartChange;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (isStartChange)
+            TextChange();
+    }
+
+    public void TextChange()
+    {
+        ScoreText.text = Save.GollMeter.ToString() + "m";
+    }
+}
